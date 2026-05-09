059182aaf84de418134030351a3b2fd1f374528a
aftandilmmd <aftandil.mmd@gmail.com>
2026-05-09 16:16:29 +0300

    refactor(db): add index to key column in field_translations migration

--- database/migrations/2025_03_25_180012_create_field_translations_table.php
+++ database/migrations/2025_03_25_180012_create_field_translations_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->string('model_type');
             $table->unsignedBigInteger('model_id');
             $table->string('locale')->index();
-            $table->string('key');
+            $table->string('key')->index();
             $table->text('value')->nullable();
             $table->timestamps();
             $table->softDeletes();
