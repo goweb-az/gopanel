218184dad757f42e329e978c0d58bf25590cbdf3
aftandilmmd <aftandil.mmd@gmail.com>
2026-05-11 12:49:05 +0300

    feat(modal): animated right-side drawer with backdrop dismiss
    Replace bootstrap modal chrome inside <x-gopanel.modal> with a dedicated
    gp-modal BEM component:
    - side drawer slides in/out from the right (cubic-bezier, 320/250ms)
    - separate backdrop element fades in and is click-to-close
    - body scroll locks while a drawer is open
    - centered variant retained for non-side modals (fade only)

--- resources/views/components/gopanel/modal.blade.php
+++ resources/views/components/gopanel/modal.blade.php
@@ -24,34 +24,51 @@
     x-on:open-modal.window="if ($event.detail?.name === '{{ $name }}') isOpen = true"
     x-on:close-modal.window="if ($event.detail?.name === '{{ $name }}') isOpen = false"
     x-on:keydown.escape.window="isOpen = false"
+    x-effect="document.body.classList.toggle('gp-modal-open', isOpen)"
 >
+    {{-- Backdrop: click to close, fade transition --}}
     <div
-        class="modal fade"
-        :class="isOpen ? 'show d-block' : ''"
+        class="gp-modal-backdrop"
         x-show="isOpen"
-        x-transition.opacity
+        x-transition.opacity.duration.250ms
+        x-on:click="isOpen = false"
+        x-cloak
+    ></div>
+
+    {{-- Drawer panel: slides from right --}}
+    <div
+        class="gp-modal {{ $side ? 'gp-modal--side' : 'gp-modal--center' }} gp-modal--{{ $size }}"
+        x-show="isOpen"
+        @if ($side)
+            x-transition:enter="gp-modal-slide-enter"
+            x-transition:enter-start="gp-modal-slide-enter-start"
+            x-transition:enter-end="gp-modal-slide-enter-end"
+            x-transition:leave="gp-modal-slide-leave"
+            x-transition:leave-start="gp-modal-slide-leave-start"
+            x-transition:leave-end="gp-modal-slide-leave-end"
+        @else
+            x-transition.opacity.duration.200ms
+        @endif
         tabindex="-1"
         aria-modal="true"
         role="dialog"
-        style="background: rgba(0,0,0,.5);"
+        x-cloak
     >
-        <div class="modal-dialog {{ $side ? 'modal-right-side' : 'modal-dialog-centered' }} {{ $sizeClass }}">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title">{{ $title }}</h5>
-                    <button type="button" class="btn-close" x-on:click="isOpen = false" aria-label="Close"></button>
-                </div>
-
-                <div class="modal-body">
-                    {{ $slot }}
-                </div>
-
-                @isset($footer)
-                    <div class="modal-footer">
-                        {{ $footer }}
-                    </div>
-                @endisset
+        <div class="gp-modal__content">
+            <div class="gp-modal__header">
+                <h5 class="gp-modal__title">{{ $title }}</h5>
+                <button type="button" class="btn-close" x-on:click="isOpen = false" aria-label="Close"></button>
             </div>
+
+            <div class="gp-modal__body">
+                {{ $slot }}
+            </div>
+
+            @isset($footer)
+                <div class="gp-modal__footer">
+                    {{ $footer }}
+                </div>
+            @endisset
         </div>
     </div>
 </div>
